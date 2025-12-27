115c431105b3fc4a35bc0aaff1bf82dc3042b687
sredowan <aarsayem002@gmail.com>
2025-12-27 13:32:14 +0600

    chore: prepare for hostinger v1 deployment

--- api/_lib/db/index.ts
+++ api/_lib/db/index.ts
@@ -7,6 +7,7 @@ dotenv.config();
 
 const pool = new pg.Pool({
     connectionString: process.env.DATABASE_URL,
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined
 });
 
 export const db = drizzle(pool, { schema });
